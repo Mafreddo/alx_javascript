df4b04f66c0a0381d0ce87bbcb77bf0c7652d60d
Mafreddo <sello.mathekga334@gmail.com>
2024-02-20 12:44:35 +0200

    JS

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@ let name0 = 'C is fun';
 let name1 = 'Python is cool';
 let name2 = 'JavaScript is amazing';
 
-console.log(name0,name1,name2 )
+console.log(name0)
+console.log(name1)
+console.log(name2)
