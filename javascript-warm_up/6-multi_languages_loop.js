17597759fd213feae78b2cd6c0f9f5727392519e
Mafreddo <sello.mathekga334@gmail.com>
2024-02-20 13:12:30 +0200

    This is jave

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 whwile(true){
     //continue to the next step
 }
-
+    for (int i=0; i<1; i++)
 let statements =['C is fun', 'Python is cool', 'JavaScript is amazing']
 console.log(statement[0, 1 , 2])
