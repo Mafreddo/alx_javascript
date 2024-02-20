09f4c32685df52dabef7afe1d08b82f84357ff6b
Mafreddo <sello.mathekga334@gmail.com>
2024-02-20 16:39:37 +0200

    as well

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,4 @@
-let i ="Where i am"
-for (let i=0;i<= 5; i++){
-    console.log(i);
+let statements =["Where i am"]
+    for (let i=0; i<statements.length; i++){
+        console.log(statements[i])
 }
