3d3165da91d887a719737efae94724a6dc81533d
Mafreddo <sello.mathekga334@gmail.com>
2024-02-20 13:14:44 +0200

    java

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 whwile(true){
     //continue to the next step
 }
-    for (int i=0; i<1; i++)
 let statements =['C is fun', 'Python is cool', 'JavaScript is amazing']
-console.log(statement[0, 1 , 2])
+console.log(statement[0, 1 , 2]){
+    for (int i=0; i<1; i++)
+}
