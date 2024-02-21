65e9fa280486af09f79a53798eeacc5e5b56a762
Mafreddo <sello.mathekga334@gmail.com>
2024-02-21 10:08:38 +0200

    change

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle{
         height = h
     }
 }
-const Rectangle = new Rectangle(3, 3);
+const Rectangle = new Rectangle(w, h);
 console.log(Rectangle);
 console.log(Rectangle.w);
 console.log(Rectangle.h);
