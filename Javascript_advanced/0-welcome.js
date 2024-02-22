a223d1d05a7955d9a17bd36b79203999e9fb500a
Mafreddo <sello.mathekga334@gmail.com>
2024-02-22 13:01:04 +0200

    Lets begin

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,10 @@
+// Creating a fuction that takes two aregumets
+
+function welcome(){
+firstname = "Freddy"
+lastname = "Mathekga"
+
+Fullname = firstname + lastname
+}
+
+console.log(welcome + " "+ desplayFullname)
---
+++ Javascript_advanced/README.md
@@ -0,0 +1,1 @@
+This is JavaScript advanced course.
