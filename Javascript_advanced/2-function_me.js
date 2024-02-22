adaaea92361e815a08c96ba0c19401a8d85ef742
Mafreddo <sello.mathekga334@gmail.com>
2024-02-22 14:00:46 +0200

    Progress

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+// Doing Closures
+
+function welcomeMessage(){
+    fullname = "firstnema + lastname";
+
+}
+let guillaume = welcomMessage("Guillaume");
+let alex = welcomMessage("Alex");
+let fred = welcomeMessage("Fred");
+
+console.log(fullname);
